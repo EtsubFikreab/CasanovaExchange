a16156d737f51fea2aa4eb03a4ec5dd247b7e719
Amanuelm <75570250+Amanuelm@users.noreply.github.com>
2023-05-30 08:39:45 +0300

    remove duplicated files

--- CasanovaExchange/Data/Scripts/add haricot bean.sql
+++ CasanovaExchange/Data/Scripts/add haricot bean.sql
@@ -2,9 +2,19 @@ use commodityexchangedb;
 
 insert into commodity (Name, Symbol, [Description], CommodityWarehouseWarehouseId, ProductionYear)
 values 
-    ('Haricot bean', 'RWPALG','Round White Pea beans A',51,2015),
-    ('Haricot bean', 'RWPALG','Round White Pea beans A',57,2015),
-    ('Haricot bean', 'RWPA4','Round White Pea beans A',57,2015)
+    ('Haricot bean', 'RWPA','Round White Pea beans A',1,2015),
+    ('Haricot bean', 'RWPA','Round White Pea beans A',2,2015),
+    ('Haricot bean', 'RWPB','Round White Pea beans B',1,2015),
+    ('Haricot bean', 'RWPB','Round White Pea beans B',2,2015),
+    ('Haricot bean', 'RWPC','Round White Pea beans C',1,2015),
+    ('Haricot bean', 'RWPC','Round White Pea beans C',2,2015),
+    ('Haricot bean', 'FWPA','Flat White Pea beans A',1,2015),
+    ('Haricot bean', 'FWPA','Flat White Pea beans A',2,2015),
+    ('Haricot bean', 'FWPB','Flat White Pea beans B',1,2015),
+    ('Haricot bean', 'FWPB','Flat White Pea beans B',2,2015),
+    ('Haricot bean', 'FWPC','Flat White Pea beans C',1,2015),
+    ('Haricot bean', 'FWPC','Flat White Pea beans C',2,2015);
+    
 
 SELECT * from Commodity
 SELECT * FROM AspNetUsers
