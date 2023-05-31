cc3d27ce890387e8aafac822e42d7010c53dfe89
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2023-05-31 21:10:56 +0300

    revert changes

--- CasanovaExchange/ViewModel/CommodityListingViewModel.cs
+++ CasanovaExchange/ViewModel/CommodityListingViewModel.cs
@@ -6,6 +6,6 @@ namespace CasanovaExchange.ViewModel
 		public List<CommodityListing>? CommodityListings { get; set; }
 		public CommodityListing? CommodityListing { get; set; }
 		public double? Quantity { get; set; }
-		public Commodity? commodity { get; set; }
+		public Commodity? commodity { get; set; } 
 	}
 }
