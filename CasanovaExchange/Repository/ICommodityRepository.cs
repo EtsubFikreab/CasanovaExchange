29d78b6cacf7bc88ebf46e934536adf8cd966b8f
Etsub Fikreab <76394016+EtsubFikreab@users.noreply.github.com>
2023-06-01 20:14:11 +0300

    buy commodity

--- CasanovaExchange/Repository/ICommodityRepository.cs
+++ CasanovaExchange/Repository/ICommodityRepository.cs
@@ -1,18 +1,20 @@
 ﻿using CasanovaExchange.Models;
+using CasanovaExchange.ViewModel;
 
 namespace CasanovaExchange.Repository
 {
     public interface ICommodityRepository
     {
-        public bool buyCommodity(int id, double PurchaseQuantity, string userId);
-        public List<Warehouse> GetWarehouseList();
+        public bool BuyCommodity(CommodityListingViewModel commodityListingViewModel, string userId);
+		public List<Warehouse> GetWarehouseList();
         public List<string> GetCommodityList();
         public List<Commodity> GetCommodityByName(string commodityName);
         public void CheckPortfolio(string UserId);
         public Wallet GetWallet(string CurrentUserId);
         public void AddBalance(string CurrentUserId, double Balance);
-        public Portfolio GetPortfolio();
+        public Portfolio GetPortfolio(string CurrentUserId);
         public Commodity GetCommodityById(int id);
         public List<CommodityListing> GetCommodityListings(Commodity commodity);
+        public void SellCommodity(CommodityListingViewModel commodityListingViewModel);
 	}
 }
